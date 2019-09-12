6347f053e91987126bae27f0e8c799d45664b337
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-12 11:40:56 -0400

    Populated Setup with a form

--- front-end/src/components/Setup.js
+++ front-end/src/components/Setup.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 
 class Setup extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            budget: 0
+        }
+    }
+
+    changeName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    changeBudget = (e) => {
+        this.setState({
+            budget: e.target.value
+        })
+    }
+ 
+    submitSetup = (e) => {
+        e.preventDefault();
+        console.log('Budget:', this.state.budget, '\nName:', this.state.name)
+    }
+
     render() { 
         return ( 
             <div className='col col-sm-12'>
                 <form onSubmit={this.submitSetup}>
                     <div className='form-group'>
-                        <label for='name'>Name</label>
-                        <input type='text' className='form-control' id='name' />
+                        <label htmlFor='name'>Name</label>
+                        <input type='text' value={this.state.name} onChange={this.changeName} className='form-control' id='name' />
                     </div>
 
                     <div className='form-group'>
-                        <label for='total-budget'>What is your budget?</label>
-                        <input type='text' className='form-control' id='total-budget' />
+                        <label htmlFor='total-budget'>What is your budget?</label>
+                        <input type='number' value={this.state.budget} onChange={this.changeBudget} className='form-control' id='total-budget' />
                     </div>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
                 </form>
             </div>
         );
